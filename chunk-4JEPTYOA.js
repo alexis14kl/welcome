32f63f47f2828d853b84b81a6605c2ee71be72de
alexis14kl <rapalexism@gmai.com>
2024-12-11 00:04:47 -0500

    response 2

---
+++ chunk-4JEPTYOA.js
@@ -0,0 +1,1 @@
+import"./chunk-4CLCTAJ7.js";var n=[{path:"",redirectTo:"index",pathMatch:"full"},{path:"index",loadComponent:()=>import("./chunk-2FSLAER7.js").then(t=>t.BeginningComponent),data:{title:"Inicio"}}];export{n as routes};
