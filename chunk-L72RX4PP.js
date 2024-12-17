8a8a12d2c98702e9eef0292871ab422eb36bd759
alexis14kl <rapalexism@gmai.com>
2024-12-16 21:13:35 -0500

    tecnologias en portafolio

---
+++ chunk-L72RX4PP.js
@@ -0,0 +1,1 @@
+import"./chunk-GAL4ENT6.js";var o=[{path:"",redirectTo:"",pathMatch:"full"},{path:"",loadComponent:()=>import("./chunk-D4PAR73C.js").then(t=>t.IndexComponent),data:{title:"Portafolio"}},{path:"contacto",loadComponent:()=>import("./chunk-KFLNSUG4.js").then(t=>t.ContactComponent),data:{title:"Contacto"}}];export{o as routes};
