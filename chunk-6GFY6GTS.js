add1e07edd6b169ce24bb0ca8fb03504ddca635d
alexis14kl <rapalexism@gmai.com>
2024-12-24 17:45:45 -0500

    correcion img compose

---
+++ chunk-6GFY6GTS.js
@@ -0,0 +1,1 @@
+import"./chunk-GAL4ENT6.js";var o=[{path:"",redirectTo:"",pathMatch:"full"},{path:"",loadComponent:()=>import("./chunk-G64KWEGA.js").then(t=>t.IndexComponent),data:{title:"Portafolio"}},{path:"contacto",loadComponent:()=>import("./chunk-TH5WJNLO.js").then(t=>t.ContactComponent),data:{title:"Contacto"}}];export{o as routes};
