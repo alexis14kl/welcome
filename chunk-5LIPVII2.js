07b53103e88344cea1937d5ebb81216f69c0443c
alexis14kl <rapalexism@gmai.com>
2024-12-28 15:06:01 -0500

    modificacion del menu bar header 2

---
+++ chunk-5LIPVII2.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-2NUQYDST.js";import"./chunk-4XEZ7QW3.js";import"./chunk-54F5PXIR.js";import"./chunk-GAL4ENT6.js";export{a as BeginningComponent};
---
+++ chunk-FSLKYIVH.js
@@ -0,0 +1,1 @@
+import"./chunk-GAL4ENT6.js";var n=[{path:"",redirectTo:"index",pathMatch:"full"},{path:"index",loadComponent:()=>import("./chunk-5LIPVII2.js").then(t=>t.BeginningComponent),data:{title:"Inicio"}}];export{n as routes};
