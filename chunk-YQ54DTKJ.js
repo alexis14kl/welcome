ecd5cbe7c7b482dd5620ecdd5129acafe01923cc
alexis14kl <rapalexism@gmail.com>
2025-05-06 20:45:27 +0200

    add fragmentos de compativilidad en navegadores

---
+++ chunk-YQ54DTKJ.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-LZCMB235.js";import"./chunk-4QHEB22J.js";import"./chunk-Q63GRA4T.js";import"./chunk-GAL4ENT6.js";export{a as BeginningComponent};
--- chunk-ZO7UAQ7C.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-3ZLVK4K3.js";import"./chunk-4QHEB22J.js";import"./chunk-Q63GRA4T.js";import"./chunk-GAL4ENT6.js";export{a as BeginningComponent};
